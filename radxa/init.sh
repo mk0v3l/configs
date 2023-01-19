852e0c3017566ef4ad5e2f4681bf1f23635b840c
Mickael Kovel <90647742+mk0v3l@users.noreply.github.com>
2023-01-19 14:24:38 +0100

    Update init.sh

--- radxa/init.sh
+++ radxa/init.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 sudo apt -y update
 sudo apt -y upgrade
 
@@ -5,9 +6,9 @@ sudo nmcli dev wifi connect "Proximus-Home-C098" password "wpzpc7anzwmzc"
 sudo systemctl edit usb_interface_p.service --full --force
 systemctl enable usb_interface_p.service
 
-sudo apt install ubuntu-mate-desktop
+#sudo apt install ubuntu-mate-desktop
 #sudo apt install -y xfce4
-
+sudo apt install plasma-desktop 
 
 sudo apt install -y libfltk1.3-dev
 sudo apt install -y g++-10
